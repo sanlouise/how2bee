82d1a87f5273b9514d27e53ed4f8eca028fd3183
kgadams93 <kgadams93@gmail.com>
2016-04-04 23:21:44 +0000

    Generate users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You signed up successfully."
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Sign up form is invalid."
+      flash[:color] = "invalid"
+    end
+    render "new"
   end
 end
