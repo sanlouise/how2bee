3580730e9676e074ba70a0d507c7d8a3ba5e9de6
kgadams93 <kgadams93@gmail.com>
2016-04-05 00:46:57 +0000

    Create index and show pages for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:user])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "You signed up successfully."
       flash[:color] = "valid"
@@ -14,4 +23,18 @@ class UsersController < ApplicationController
     end
     render "new"
   end
+  
+  def update
+    @user = User.find(params[:user])
+  end
+  
+  def destroy
+    @user = User.find(params[:user])
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   get 'users/new'
 
+  resources :users
   resources :articles
   root 'pages#home'
   
