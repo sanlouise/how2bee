ce4bbc761ef438a6cbded2aa188671e7ac2107e0
kgadams93 <kgadams93@gmail.com>
2016-04-05 23:28:59 +0000

    Add admin? functionality to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405230043) do
+ActiveRecord::Schema.define(version: 20160405232631) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20160405230043) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin?",          default: false, null: false
   end
 
 end
