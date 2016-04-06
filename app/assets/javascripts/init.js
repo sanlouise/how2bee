619788754286215bed5a1712db40a9ad8dc54913
kgadams93 <kgadams93@gmail.com>
2016-04-06 23:45:09 +0000

    Fix new article problem to input multiple lines of content for :content key

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
   // Initialize collapse button
   $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
-  $('.parallax').parallax();
-  $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
-  $('select').material_select();
 });
